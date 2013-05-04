25c286a17e9f5ed78a3fa4f68f45a5a25d0f2e34
glvnst <benjaminburke@me.com>
2013-05-04 04:13:24 -0700

    Packaging prep

--- setup.py
+++ setup.py
@@ -2,13 +2,25 @@
 """ Setup the setup """
 from setuptools import setup
 
+LONG_DESCRIPTION = """
+usersettings
+------------
+"usersettings" is a python module for easily managing persistent settings
+using an editable format and stored in an OS-appropriate location
+(windows/os x/linux are supported).
+
+For more information, please refer to README.md or the `usersettings github
+page <https://github.com/glvnst/usersettings>`_.
+"""
+
 setup(
-    name = "UserSettings",
-    version = "1.0",
-    description = "Portable Local Settings Storage",
-    url = "https://github.com/glvnst/usersettings",
+    name = "usersettings",
+    version = "1.0.3",
     author = "Ben Burke",
     author_email = "benburke42@gmail.com",
+    url = "https://github.com/glvnst/usersettings",
+    description = "Portable Local Settings Storage",
+    long_description = LONG_DESCRIPTION,
     classifiers = [
         "Programming Language :: Python",
         "License :: OSI Approved :: BSD License",
@@ -18,6 +30,10 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
+    platforms = ["any"],
+    license = "https://raw.github.com/glvnst/usersettings/master/LICENSE.txt",
+
     py_modules=['usersettings'],
+    package_data = {'': ['*.txt', '*.md']},
     install_requires = ['appdirs>=1.2'],
 )
